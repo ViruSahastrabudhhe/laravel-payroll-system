894d53c1da6799e2f3785a0afe4d4f072a7ff9e2
ViruSahastrabudhhe <s21contactemail@gmail.com>
2026-02-19 00:14:03 +0800

    Adds auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Auth::routes();
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
